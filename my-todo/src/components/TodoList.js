9ae49864d5bf6a9aa20f78909cf76f56fa30fc58
Ahmad Mostafa <124518625+ahmadalasiri@users.noreply.github.com>
2025-04-24 17:10:18 +0200

    ..

--- my-todo/src/components/TodoList.js
+++ my-todo/src/components/TodoList.js
@@ -1,20 +1,48 @@
-import React, { useState } from "react";
-import TodoItem from "./TodoItem";
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ToggleButton from "@mui/material/ToggleButton";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
 
-function TodoList({ todos, toggleTodo, deleteTodo, editTodo }) {
+export default function TodoList() {
   return (
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          toggleTodo={toggleTodo}
-          deleteTodo={deleteTodo}
-          editTodo={editTodo}
-        />
-      ))}
-    </div>
+    <Container maxWidth="sm">
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography
+            gutterBottom
+            sx={{ color: "text.secondary", fontSize: 14 }}
+          >
+            My Todo List
+          </Typography>
+          {/* Filter buttons */}
+          <ToggleButtonGroup
+            // value={alignment}
+            exclusive
+            // onChange={handleAlignment}
+            aria-label="text alignment"
+          >
+            <ToggleButton value="left" aria-label="left aligned">
+              <FormatAlignLeftIcon />
+            </ToggleButton>
+            <ToggleButton value="center" aria-label="centered">
+              <FormatAlignCenterIcon />
+            </ToggleButton>
+            <ToggleButton value="right" aria-label="right aligned">
+              <FormatAlignRightIcon />
+            </ToggleButton>
+            <ToggleButton value="justify" aria-label="justified" disabled>
+              <FormatAlignJustifyIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </CardContent>
+      </Card>
+    </Container>
   );
 }
-
-export default TodoList;
